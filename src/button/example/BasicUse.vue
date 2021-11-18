091991446d68c8851649390992504be63731b085
DaiSX <>
2021-11-18 23:13:22 +0800

    +)init

---
+++ src/button/example/BasicUse.vue
@@ -0,0 +1,14 @@
+<template>
+  <do-button>组件库从我开始</do-button>
+</template>
+
+<script>
+import Button from '../Button'
+
+export default {
+  name: 'BasicUse',
+  components: {
+    [Button.name]: Button
+  }
+}
+</script>
---
+++ src/button/example/ModifyColor.vue
@@ -0,0 +1,14 @@
+<template>
+  <do-button color="#03A9F4">do-it-ui</do-button>
+</template>
+
+<script>
+import Button from '../Button'
+
+export default {
+  name: 'ModifyColor',
+  components: {
+    [Button.name]: Button
+  }
+}
+</script>
